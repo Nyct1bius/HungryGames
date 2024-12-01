cb1d61e1f639339b594e4ebaa65f4cc5e43ed5a4
Kayke Celli <Kaykecelli6@gmail.com>
2024-12-01 18:46:55 -0300

    changed to networkbehaviour

--- Assets/Scripts/NPC/BuffObject.cs
+++ Assets/Scripts/NPC/BuffObject.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class BuffObject : MonoBehaviour
+public class BuffObject : NetworkBehaviour
 {
     private float enabledTimer = 15f;
 
--- Assets/Scripts/NPC/DebuffObject.cs
+++ Assets/Scripts/NPC/DebuffObject.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class DebuffObject : MonoBehaviour
+public class DebuffObject : NetworkBehaviour
 {
     private float enabledTimer = 15f;
 
